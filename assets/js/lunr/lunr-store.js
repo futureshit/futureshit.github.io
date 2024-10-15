1c86d5a81b437fb44e7c125aaf915ef46f001302
alice <alice@c3d2.de>
2024-10-16 00:00:59 +0200

    2024

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "Riseup",
+        "excerpt":"Rise up! Erhebt euch, ihr habt nichts zu verlieren außer euren Stacheldrahtzäunen! Der technologisierte Kapitalismus des 21. Jahrhunderts erweitert und stabilisiert den durch die industrielle Revolution des 19. Jahrhunderts geprägten Kapitalismus. Fords und Taylors Errungenschaften um die Präszisierung der Arbeit und Einschränkung der Autonomie der Arbeiter*innen werden durch neue Technologien...","categories": ["aufruf"],
+        "tags": [],
+        "url": "/aufruf/riseup/",
+        "teaser": null
+      },{
+        "title": "Call",
+        "excerpt":"#Anarchist Hackers Basis dieses Textes war ein Post auf hispagatos.org, welcher von uns übertragen, übersetzt &amp; ergänzt wurde. ##Der nächste Schritt anarchistischer Praktik: anarchistische Hacker*innen! Es gab eine Zeit des friedlichen Anarchismus und Menschen beteiligten sich in der ersten Internationalen. Kluge Köpfe und Freigeister aus Frankreich, Spanien, Deutschland und der...","categories": ["aufruf"],
+        "tags": [],
+        "url": "/aufruf/call/",
+        "teaser": null
+      },{
+        "title": "Passwords",
+        "excerpt":"Sichere Passwörter Ein Passwort hier, ein Passwort da. Überall sind wir auf Passwörter angewiesen, egal ob bei der Bank, Krankenkasse, Email oder auf Arbeit. Wir müssen uns eine Menge merken, denn: niemals ein Passwort mehrfach verwenden! Wird zB. ein Onlineshop gehackt, bei dem du mal bestellt hast, dann kann dabei...","categories": ["anleitung"],
+        "tags": ["passwörter","sicherheit"],
+        "url": "/anleitung/passwords/",
+        "teaser": null
+      },{
+        "title": "Adblock",
+        "excerpt":"Adblock Auf fast jeder Website ist man Werbung ausgesetzt und nicht nur das einem einfach lästige Produkte vorgeschlagen werden, sondern man bekommt von Algorithmen persönlich zugeschnittene Inhalte. Denn wir werden überall wo wir unsere Daten hinterlassen, egal ob bei Instagram oder Onlineshops, dauerhaft ausgewertet und unsere Daten weiterverkauft. Warum? Weil...","categories": ["anleitung"],
+        "tags": [],
+        "url": "/anleitung/adblock/",
+        "teaser": null
+      },{
+        "title": "Dns",
+        "excerpt":"Sicherer DNS Wenn im Internet Adressen angefragt werden, zB. durch Ansurfen im Browser, wird der Name (instagram.com) zur der dahinter liegenden IP-Adresse eines Servers aufgelöst. Dabei werden die Anfragen zur Auflösung über idr. öffentliche Server gestellt, die die Zuordnung vornehmen. Das “Domain Name System” ist quasi wie ein Telefonbuch. Über...","categories": ["anleitung"],
+        "tags": [],
+        "url": "/anleitung/dns/",
+        "teaser": null
+      },{
+        "title": "Browser",
+        "excerpt":"Browser Best Practice Anmerkung zur Wahl des Browsers Chrome ist mit Abstand der schnellste Browser, aber auch der am meißten “nach Hause” telefonierende Chrome bzw. Google arbeitet stark mit der Werbeindustrie zusammen &amp; versucht Adblocker auszubremsen Edge basiert auf Chrome aber statt zu Google telefoniert er halt zu Microsoft Firefox...","categories": ["anleitung"],
+        "tags": [],
+        "url": "/anleitung/browser/",
+        "teaser": null
+      }]
